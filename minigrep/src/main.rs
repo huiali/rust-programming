349d443dceaa80aa8653c26f8c028b83dc474d0a
huiali <huiali@hotmail.com>
2021-01-08 15:55:03 +0800

    feat: Using result to handle errors

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,36 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
-    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    print!("With text :\n{}", content);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments:{}", err);
+        process::exit(1);
+    });
+    println!(
+        "Searching for {} and in file {}",
+        config.query, config.filename
+    );
 
-    // println!("Searching for {} and in file {}", query, filename);
+    let content =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    print!("With text :\n{}", content);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
