fcaf76f2710a8b8642e00eb7c764476738e7354c
huiali <huiali@hotmail.com>
2021-01-08 13:25:10 +0800

    feat: read file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,17 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let (query, filename) = parse_config(&args);
+    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    print!("With text :\n{}", content);
 
+    // println!("Searching for {} and in file {}", query, filename);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let filename = &args[2];
-
-    println!("Searching for {} and in file {}", query, filename);
+    (query, filename)
 }
