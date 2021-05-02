e99e76655dfc921a94c9d8e7d15494f285b192bc
huiali <huiali@hotmail.com>
2021-05-02 14:18:48 +0800

    feat: control-flow

--- common-programming-concepts/control-flow/src/main.rs
+++ common-programming-concepts/control-flow/src/main.rs
@@ -1,3 +1,73 @@
 fn main() {
-    println!("Hello, world!");
+    // if 表达式
+
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // 在 let 语句中使用 if,  变量必须只有一个类型
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
+
+    // 使用循环重复执行
+    // Rust 有三种循环：loop、while 和 for。我们每一个都试试。
+
+    // loop
+
+    // loop {
+    //     println!("again!");
+    // }
+
+    // 从循环返回
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    // while 条件循环
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index = index + 1;
+    }
+
+    // 使用 for 遍历集合
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    // rev，用来反转 range：
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
