97d80add80c4ea93fca1e486cb75b5d3f959b9f1
huiali <huiali@hotmail.com>
2021-05-07 22:29:40 +0800

    feat: Option<T>,_

--- enums/match-control/src/main.rs
+++ enums/match-control/src/main.rs
@@ -1,13 +1,31 @@
 fn main() {
     let t = value_in_cents(Coin::Penny);
     println!("the value is {:?}", t);
+
+    let a = value_in_cents(Coin::Quarter(UsState::Alabama));
+    println!("the value is {:?}", a);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // _ 通配符
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -18,6 +36,25 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+#[derive(Debug)] // 这样可以可以立刻看到州的名称
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+// 匹配 Option<T>
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
