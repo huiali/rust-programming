2b2f47460b600ae5b39cbb4a64737d563252f31b
huiali <huiali@hotmail.com>
2021-05-07 22:23:57 +0800

    feat: Init if-let-control

--- enums/match-control/src/main.rs
+++ enums/match-control/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let t = value_in_cents(Coin::Penny);
+    println!("the value is {:?}", t);
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
