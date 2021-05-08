3aa843ff0f1059b5c88f90691dda73475d39c275
huiali <huiali@hotmail.com>
2021-05-08 22:40:06 +0800

    feat: if let control

--- enums/if-let-control/src/main.rs
+++ enums/if-let-control/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    let mut count = 0;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)] // 这样可以可以立刻看到州的名称
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
