92695eebc28c2d004d0bdf7d5d4fb026683aa9a9
huiali <huiali@hotmail.com>
2021-05-10 21:28:15 +0800

    feat: hasmap

--- collections/hashmap/src/main.rs
+++ collections/hashmap/src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    // 新建一个哈希 map
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // 使用 collect 方法将这个元组 vector 转换成一个 HashMap
+    let teams = vec![
+        String::from("Blue"),
+        String::from("Yellow"),
+        String::from("Red"),
+    ];
+    let initial_scores = vec![10, 50, 35, 65];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    println!("scores is {:#?}", scores);
+
+
+    
 }
