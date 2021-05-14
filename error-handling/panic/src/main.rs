cfebcd61b7e29308770dfc78a7fdc97c3057ae7f
huiali <huiali@hotmail.com>
2021-05-14 22:48:13 +0800

    feat: panic!

--- error-handling/panic/src/main.rs
+++ error-handling/panic/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3];
+
+    v[99];  // 缓冲区溢出
+
+    // panic!("crash and burn");
+
+    // backtrace 是一个执行到目前位置所有被调用的函数的列表。
+    // cmd $env:RUST_BACKTRACE=1
 }
