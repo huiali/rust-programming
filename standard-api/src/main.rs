c0475fa7ad535ff9f3219a469bd4edd687ebf5b6
huiali <huiali@hotmail.com>
2021-05-29 23:12:44 +0800

    feat: standard api

---
+++ standard-api/src/main.rs
@@ -0,0 +1,40 @@
+// mod middleware;
+mod todo;
+mod constants;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use anyhow::Result;
+use sqlx::mysql::MySqlPoolOptions;
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let database_url = "mysql://root@localhost:3306/todos"; // &env::var("DATABASE_URL")?;
+    let pool = MySqlPoolOptions::new().connect(&database_url).await?;
+
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+            .configure(todo::init) // init todo routes
+    })
+    .keep_alive(3600)
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
+
+    Ok(())
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
---
+++ standard-api/src/todo/routes.rs
@@ -0,0 +1,51 @@
+use crate::todo::Todo;
+use actix_web::{get, web, HttpResponse, Responder};
+use sqlx::MySqlPool;
+
+#[get("/todos")]
+async fn find_all(db_pool: web::Data<MySqlPool>) -> impl Responder {
+    let result = Todo::find_all(db_pool.get_ref()).await;
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        _ => HttpResponse::BadRequest().body("Error trying to read all todos from database"),
+    }
+}
+
+// #[get("/todo/{id}")]
+// async fn find(id: web::Path<i32>, db_pool: web::Data<MySqlPool>) -> impl Responder {
+//     let result = Todo::find_by_id(id.into_inner(), db_pool.get_ref()).await;
+//     match result {
+//         Ok(todo) => HttpResponse::Ok().json(todo),
+//         _ => HttpResponse::BadRequest().body("Todo not found"),
+//     }
+// }
+
+// #[post("/todo")]
+// async fn create(todo: web::Json<TodoRequest>, db_pool: web::Data<MySqlPool>) -> impl Responder {
+//     let result = Todo::create(todo.into_inner(), db_pool.get_ref()).await;
+//     match result {
+//         Ok(todo) => HttpResponse::Ok().json(todo),
+//         _ => HttpResponse::BadRequest().body("Error trying to create new todo"),
+//     }
+// }
+
+// #[put("/todo/{id}")]
+// async fn update(
+//     id: web::Path<i32>,
+//     todo: web::Json<TodoRequest>,
+//     db_pool: web::Data<MySqlPool>,
+// ) -> impl Responder {
+//     let result = Todo::update(id.into_inner(), todo.into_inner(), db_pool.get_ref()).await;
+//     match result {
+//         Ok(todo) => HttpResponse::Ok().json(todo),
+//         _ => HttpResponse::BadRequest().body("Todo not found"),
+//     }
+// }
+
+// function that will be called on new Application to configure routes for this module
+pub fn init(cfg: &mut web::ServiceConfig) {
+    cfg.service(find_all);
+    // cfg.service(find);
+    // cfg.service(create);
+    // cfg.service(update);
+}
